afccd7ad2d9bbaf4aa350190040759ceb2bef032
ggDiscordgg <ahtraf1223@gmail.com>
2025-02-07 10:52:48 +0300

    adslfsldf

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 
-// اتصال MongoDB
+// اتصال MongoDBdf
 console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("✅ Connected to MongoDB"))
